1eb99f403180b21402b44dbf89a89980b75b2a70
Leozu09 <zunigafelix58@gmail.com>
2025-10-20 21:55:08 -0500

    main working and master created

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template,request,session,redirect,url_for,g
+from bd import get_connection
+from flask_socketio import SocketIO
+
+app = Flask(__name__, template_folder="./App/Templates", static_folder="./App/Static")
+app.secret_key = 'clave_super_secreta_123'
+
+socketio = SocketIO(app)
+
+@app.route("/")
+@app.route("/RoomFlow")
+def Index():
+    return render_template("Index.html")
+
+
+# Iniciar el servidor
+if __name__ == "__main__":
+    socketio.run(app, host='0.0.0.0', port=8000, debug=True)
+
