592a1a45dc93bb66eb4284e0f13c483fb22627eb
Leozu09 <zunigafelix58@gmail.com>
2025-10-20 23:38:06 -0500

    booking controller and route

---
+++ App/Blueprints/ModuleRooms&Booking/RoutesBooking.py
@@ -0,0 +1,11 @@
+from flask import render_template, Blueprint, request, jsonify, session, redirect, url_for, json, flash, send_file, current_app, make_response, current_app
+
+import App.Models.controller_room as controller_room
+
+travelviews_bp = Blueprint('bookingroom', __name__, template_folder='Templates', url_prefix='/BookingRoom')
+
+@travelviews_bp.route('/', methods=['GET'])
+@travelviews_bp.route('/TravelViews', methods=['GET'])
+def BookingRoom():
+    rooms = controller_room.get_rooms()
+    return render_template("Booking.html", rooms=rooms)
--- App/Models/controller_room.py
+++ App/Models/controller_room.py
@@ -5,5 +5,26 @@ def get_rooms(limit=20, offset=0):
     connection = get_connection()
     with connection.cursor() as cursor:
         cursor.execute("""
-        
-                       """)
+            SELECT
+                r.habitacion_id,
+                r.numero,
+                r.estado,
+                r.piso_id,
+                r.categoria_id,
+                c.nombre_categoria
+                
+                FROM
+                    HABITACION C
+                INNER JOIN CATEGORIA C ON r.categoria_id = c.categoria_id
+            """, (limit,offset))
+        rooms = cursor.fetchall()
+    connection.close()
+    return rooms
+
+def count_rooms():
+    connection = get_connection()
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT COUNT(*) FROM HABITACION")
+        total = cursor.fetchone()[0]
+    connection.close()
+    return total
